1430d24971e52b72b1401326d3fd7b42a69b4289
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2024-01-21 15:23:03 -0500

    DSA problem for 21/1/24

---
+++ longestSubstring.cpp
@@ -0,0 +1,28 @@
+/* We will use the concept of Sliding Window.
+- Take two pointers to denote the start and end of the window.
+*/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+        int maxLength = 0;
+        unordered_set<char> charSet;
+        int left = 0;
+        
+        for (int right = 0; right < n; right++) {
+            if (charSet.count(s[right]) == 0) {
+                charSet.insert(s[right]);
+                maxLength = max(maxLength, right - left + 1);
+            } else {
+                while (charSet.count(s[right])) {
+                    charSet.erase(s[left]);
+                    left++;
+                }
+                charSet.insert(s[right]);
+            }
+        }
+        
+        return maxLength;
+    }
+};
