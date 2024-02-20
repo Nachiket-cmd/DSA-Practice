38ac4e415260caa0728c44a96ae8328d2fe9ee61
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2024-02-20 10:52:37 -0500

    Create patternsPractice.cpp
    Striver A2Z patterns practice

---
+++ Arrays/patternsPractice.cpp
@@ -0,0 +1,60 @@
+// Online C++ compiler to run C++ program online
+/* Tips for loops
+1. for the outer loop, count the number of lines.
+2. for the inner loop, focus on the columns, and connect them somehow to the rows.
+3. Print the content inside the inner loop.
+4. Observe symmetry .
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    // patterns practice 
+    //will go according striver patterns video
+    //pattern 1 
+    // for(int i=0; i<5;i++)
+    // {
+    //     for( int j=0;j<5;j++)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    // //pattern 2
+    // for(int i=0;i<5;i++)
+    // {
+    //     for( int j=i;j>=0;j--)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    // //pattern 3
+    // for(int i=1;i<6;i++)
+    // {
+    //     for(int j=1;j<i+1;j++)
+    //     {
+    //         cout<<j;
+    //     }
+    //     cout<<endl;
+    // }
+    
+    //pattern 4
+    // for(int i=0;i<5;i++)
+    // {
+    //     int x=1;
+    //     for(int j=0;j<(5-i);j++)
+    //     {
+    //         cout<<x;
+    //         x++;
+    //     }
+    //     cout<<endl;
+    // }
+    
+    //pattern 5
+    
+    
+    
+    
+}
