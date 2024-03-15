09b948dd6cd3d237df9d1117c62567473bf114de
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2024-03-15 11:04:45 -0400

    Update patternsPractice.cpp
    Added pattern 5 from Striver A2Z

--- Arrays/patternsPractice.cpp
+++ Arrays/patternsPractice.cpp
@@ -53,7 +53,18 @@ int main() {
     // }
     
     //pattern 5
-    
+    // for(int i=0;i<5;i++)
+    // {
+    //     for( int j=0; j<5;j++)
+    //     {
+    //         if(j<(i+1))
+    //           cout<<"*";
+    //         else{
+    //             cout<<" ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
     
     
     
