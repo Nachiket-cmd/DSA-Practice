95b6e7a056b20a42dd1b944176c9d983fe1164c1
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2024-03-15 11:06:29 -0400

    Update patternsPractice.cpp
    1. Removed unwanterd comments 
    2. Added pattern 7

--- Arrays/patternsPractice.cpp
+++ Arrays/patternsPractice.cpp
@@ -8,64 +8,84 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
-    // patterns practice 
+    //patterns practice 
     //will go according striver patterns video
     //pattern 1 
-    // for(int i=0; i<5;i++)
-    // {
-    //     for( int j=0;j<5;j++)
-    //     {
-    //         cout<<"*";
-    //     }
-    //     cout<<endl;
-    // }
+    for(int i=0; i<5;i++)
+    {
+        for( int j=0;j<5;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
     
-    // //pattern 2
-    // for(int i=0;i<5;i++)
-    // {
-    //     for( int j=i;j>=0;j--)
-    //     {
-    //         cout<<"*";
-    //     }
-    //     cout<<endl;
-    // }
+    //pattern 2
+    for(int i=0;i<5;i++)
+    {
+        for( int j=i;j>=0;j--)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
     
-    // //pattern 3
-    // for(int i=1;i<6;i++)
-    // {
-    //     for(int j=1;j<i+1;j++)
-    //     {
-    //         cout<<j;
-    //     }
-    //     cout<<endl;
-    // }
+    //pattern 3
+    for(int i=1;i<6;i++)
+    {
+        for(int j=1;j<i+1;j++)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
     
     //pattern 4
-    // for(int i=0;i<5;i++)
-    // {
-    //     int x=1;
-    //     for(int j=0;j<(5-i);j++)
-    //     {
-    //         cout<<x;
-    //         x++;
-    //     }
-    //     cout<<endl;
-    // }
+    for(int i=0;i<5;i++)
+    {
+        int x=1;
+        for(int j=0;j<(5-i);j++)
+        {
+            cout<<x;
+            x++;
+        }
+        cout<<endl;
+    }
     
     //pattern 5
-    // for(int i=0;i<5;i++)
-    // {
-    //     for( int j=0; j<5;j++)
-    //     {
-    //         if(j<(i+1))
-    //           cout<<"*";
-    //         else{
-    //             cout<<" ";
-    //         }
-    //     }
-    //     cout<<endl;
-    // }
-    
+    for(int i=0;i<5;i++)
+    {
+        for( int j=0; j<5;j++)
+        {
+            if(j<(i+1))
+              cout<<"*";
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+
+    //pattern 7 (pattern 6 used for learning)
+    for( int i=0;i<5;i++)
+    {
+        //space
+        for(int j=0; j<i;j++)
+        {
+            cout<<" ";
+        }
+        //stars
+        for(int j=0;j<2*5-(2*i)-1;j++)
+        {
+            cout<<"*";
+        }
+        //space
+        for(int j=0; j<i;j++)
+        {
+            cout<<" ";
+        }
+        cout<<endl;
+    }
     
     
 }
