ef20e810d605a90996326deb98840a99cd8bce2b
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2024-05-23 11:14:31 -0400

    Update patternsPractice.cpp
    Pattern update 2 for 23/05/2024

--- Arrays/patternsPractice.cpp
+++ Arrays/patternsPractice.cpp
@@ -129,3 +129,18 @@ int main()
     return 0;
 } 
 }
+
+//pattern 9
+{
+    int counter = 1;
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0; j<=i; j++)
+        {
+           cout<<counter<<" ";
+           counter++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
