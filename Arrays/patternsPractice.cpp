7cda40ec8f4c668d7eeeda04cf45cd460c1dd4d5
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2024-05-27 17:44:43 -0400

    Update patternsPractice.cpp
    pattern 21 done

--- Arrays/patternsPractice.cpp
+++ Arrays/patternsPractice.cpp
@@ -277,3 +277,32 @@ int main() {
     
         return 0;
     }
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    for(int i=0;i<5;i++)
+    {
+        if(i==0 || i==5-1){
+        for(int j=0;j<5;j++)
+        {
+            cout<<"*";
+        }}
+        else{
+            for(int j=0;j<5;j++)
+            {
+                if(j==0 || j==5-1)
+                {
+                    cout<<"*";
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
