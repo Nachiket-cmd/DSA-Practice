0e5c3a3fed95c1dca776036b8195586facbb3378
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2024-07-17 12:17:15 -0400

    Create bubbleSort.cpp
    logic behind bubbleSort

---
+++ Sorting/bubbleSort.cpp
@@ -0,0 +1,34 @@
+//TC- O(n^2) SC- O(1)
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i;j<n-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    bubbleSort(arr,n);
+    for(auto i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
