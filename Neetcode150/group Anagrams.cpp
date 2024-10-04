52946f770fb6ed26e06fdf8dd0630a0033214f2f
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2024-10-04 12:38:31 -0400

    Update group Anagrams.cpp

--- Neetcode150/group Anagrams.cpp
+++ Neetcode150/group Anagrams.cpp
@@ -1,4 +1,5 @@
-/* My Logic. TC : O(n) SC : O(n). I couldnt solve it so this solution is derived from Neetcode,s logic */
+/* My Logic. TC : O(n) SC : O(n). I couldnt solve it so this solution is derived from Neetcode,s logic
+use a unordered map and store the sorted string as key and then its anagrams in values. The values will be stored in a vector.*/
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
@@ -14,7 +15,7 @@ public:
         {
             temp = strs[i];
             sort(strs[i].begin(),strs[i].end());
-            mp[strs[i]].push_back(temp);
+            mp[strs[i]].push_back(temp); //inserting the sorted key.
         }
 
            //Removing the key and taking the value part inside every key.
@@ -22,7 +23,7 @@ public:
 
         for( auto i = mp.begin() ; i!=mp.end() ; i++)
            {
-              ans.push_back(i -> second);
+              ans.push_back(i -> second);  // this is how you retrieve the values from an unordered map. 
            }
         return ans;
 
