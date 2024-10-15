5178fe9f899199cf954a75fa02cf0555ebd8ce68
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2024-10-15 15:03:02 -0400

    Update SingleNumber.cpp

--- Arrays/SingleNumber.cpp
+++ Arrays/SingleNumber.cpp
@@ -1,5 +1,6 @@
 //To do this in O(nlog(n)) TC and O(1) SC, we can simply sort the array and check the number which doesnt match to its prev number.
 // I have used unordered map to solve this. Just store the frequency of each number in the form of key-value pairs and check which key has its value as 1.
+// We can also used XOR gates as logic which will be the best. TC - O(n) SC- O(1)
 class Solution {
 public:
     bool compareByValue(const std::pair<int, int>& a, const std::pair<int, int>& b) {
