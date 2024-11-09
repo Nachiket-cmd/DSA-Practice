ff9365a5f4b2bda13ce2190c1d3af8d7d3c96940
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2024-11-09 11:54:32 -0500

    Update maxCard.cpp

--- rdmp/TwoPtrSlidingWindow/maxCard.cpp
+++ rdmp/TwoPtrSlidingWindow/maxCard.cpp
@@ -1,1 +1,53 @@
+/*You are given an array and the condition is that you can only take the elements from the left side and right side but not from the middle of the array. By doing this we are looking the max sum with a given window size k*/
 
+
+//This one did not worked
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+       int lsum = 0 ,rsum = 0;
+       int lindex = 0;
+       for(int i=0 ; i<k ; i++)
+        {
+            lsum = lsum + cardPoints[i];
+            lindex++;
+        }
+        int maxSum = lsum;
+        int rindex = cardPoints.size() - 1;
+        while(lindex>=0)
+        {
+            lsum = lsum - cardPoints[lindex];
+            lindex--;
+            rsum = rsum + cardPoints[rindex];
+            rindex--;
+            maxSum = max(maxSum , (lsum+rsum));
+            if(lindex==0)break;
+        } 
+        return maxSum;
+    }
+};
+
+//And this one did worked
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+       int lsum = 0 ,rsum = 0;
+       int lindex = 0;
+       for(int i=0 ; i<k ; i++)
+        {
+            lsum = lsum + cardPoints[i];
+            lindex++;
+        }
+        int maxSum = lsum;
+        int rindex = cardPoints.size() - 1;
+        for(int i=k-1;i>=0;i--)
+        {
+            lsum -= cardPoints[i];
+            lindex--;
+            rsum += cardPoints[rindex];
+            rindex--;
+            maxSum = max(maxSum,(lsum+rsum));
+        }
+        return maxSum;
+    }
+};
