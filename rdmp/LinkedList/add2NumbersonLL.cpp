4a7830347642ce00a09edba48190349405c24672
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2024-11-28 01:07:59 +0000

    random

--- rdmp/LinkedList/add2NumbersonLL.cpp
+++ rdmp/LinkedList/add2NumbersonLL.cpp
@@ -17,5 +17,17 @@ class Node{
             next = nullptr;
         }
 };
+//Question: You will be given two Linked Lists, with the input numbers in reversed order. You are supposed to add these two numbers and give a resultant linked list
+//which contains the sum of both the number in the same reversed order.
 //*****PRO TIP:- Whenever we need to create a new NodeList and perform some operations, always choose the dummyNode concept. *****/
+
+Node* add2Numbers(Node* LL1, Node* LL2)
+{
+    Node* dummyNode = new Node(-1);
+    Node* temp1 = LL1;
+    Node* temp2 = LL2;
+    int carry =0;
+    int 
+    while(temp1!=NULL || temp2!=NULL)
+}
 
