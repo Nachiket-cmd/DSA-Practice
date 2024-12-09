1658f8df9a39f463e5b0aa0ecf1d32a19b54a322
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2024-12-09 05:48:46 +0000

    delNfromEnd done

--- rdmp/LinkedList/deleteNfromEnd.cpp
+++ rdmp/LinkedList/deleteNfromEnd.cpp
@@ -17,7 +17,7 @@ class Node {
         next = nullptr;
     }
 }
-//BRUTE FORCE
+//BRUTE FORCE   TC - O(2n)  SC- O(1)
 Node* deleteNfromEnd(Node* head, int n)
 {
     Node* temp = head;
@@ -52,6 +52,33 @@ Node* deleteNfromEnd(Node* head, int n)
 }
 
 
-//OPTIMAL SOLUTION
+//OPTIMAL SOLUTION.   TC - O(N)   SC- O(1)
+/*What is the concept about?
 // We can use the concept of fast and slow pointers. 
-/* */
+- We will take 2 pointers, fast and slow.
+- Start with fast pointer at head.
+- First move the fast pointer n(here n is the n position) nodes.
+- Then take another pointer slow which will point towards head.
+- Now, our slow pointer is at head and fast pointer is at nth node.
+- Now, move both pointers at the same time.
+- When our fast pointer will reach tail, our slow pointer will automatically be at the nth position from end. */
+
+Node* delNfromEnd (Node* head, int n)
+{
+    Node* fast = head;
+    for(int i = 0 ; i<n ;i++)
+    {
+        fast = fast->next;
+    }
+    if(fast == NULL) return head->next; //If we have been asked to delete the head
+    Node* slow = head;
+    while(fast != NULL)
+    {
+        fast = fast->next;
+        slow = slow->next;
+    }
+    Node* delNode = slow->next;
+    slow->next = slow->next->next;
+    free(delNode);
+    return head;
+}
