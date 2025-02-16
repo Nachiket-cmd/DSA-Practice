d14d107f12d552afd1d18385d1348492692b8457
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2025-02-16 19:59:36 +0000

    BFS. Only the code done. Understand and note making is left.

--- rdmp/Trees/2.TreeTraversal.cpp
+++ rdmp/Trees/2.TreeTraversal.cpp
@@ -71,3 +71,27 @@ void Postorder(node)
     Postorder(node->right);
     print(node);
 }
+
+
+//Levelorder (BFS). TC - O(n) SC - O(n)
+vector<vector<int>>Levelorder(Node* root)
+{
+   vector<vector<int>>ans;
+   if(root == NULL) return ans;
+   queue<Node*>q;
+   while(!q.empty())
+   {
+    int size = q.size();
+    vector<int>level;
+    for(int i = 0 ; i<size ; i++)
+    {
+        Node *node = q.front();
+        q.pop();
+        if(node->left != NULL) q.push(node->left);
+        if(node->right != NULL) q.push(node->right);
+        level.push_back(node->data);
+    }
+    ans.push_back(level);
+   }
+   return ans;
+}
