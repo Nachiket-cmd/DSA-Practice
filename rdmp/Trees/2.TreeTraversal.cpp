4fb8252d0d62634a29d9ed4d19d81bcf9126cf2e
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2025-02-25 15:47:45 +0000

    made a small change in level order(code fixed)

--- rdmp/Trees/2.TreeTraversal.cpp
+++ rdmp/Trees/2.TreeTraversal.cpp
@@ -74,18 +74,22 @@ void Postorder(node)
 
 
 //Levelorder (BFS). TC - O(n) SC - O(n)
+/*This is what BFS is all about.
+We will be using a queue for level order traversal. ans is used as a vec<vec>> to store the answer and return it. The rest of the logic can be understood by 
+going through the code.*/
 vector<vector<int>>Levelorder(Node* root)
 {
    vector<vector<int>>ans;  //making a vector to store and return the ans.
    if(root == NULL) return ans; // if the BT is empty, simply return nothing.
    queue<Node*>q; // queue to traverse through every level.
+   q.push(root);
    while(!q.empty()) // when the queue will be empty, it means that we have traversed the entire BT.
    {
     int size = q.size();  // the size will be used to traverse that many times in the loop, which will tell us about how many elements are there in the level.
     vector<int>level;   // vector to store each level.
     for(int i = 0 ; i<size ; i++) 
     {
-        Node *node = q.front(); //we will start from front as FIFO and we will go from left to right like a BT
+        Node* node = q.front(); //we will start from front as FIFO and we will go from left to right like a BT
         q.pop();
         if(node->left != NULL) q.push(node->left);
         if(node->right != NULL) q.push(node->right);
@@ -97,3 +101,25 @@ vector<vector<int>>Levelorder(Node* root)
 }
 
 
+<vector<vector<int>>LvlOrder(Node* root)
+{
+    if(root == NULL) return 0;
+    queue<Node*>q;
+    q = root;
+    <vector<vector<int>>ans;
+    while(!q.empty())
+    {
+        vector<int>level;
+        int size = q.size();
+        for(int i = 0 ; i<size ; i++)
+        {
+            Node* node = q.front();
+            q.pop();
+            if(node->left != NULL) q.push(node->left);
+            if(node->right != NULL) q.push(node->right);
+            level.push_back(node->data);
+        }
+        ans.push_back(level);
+    }
+    return ans;
+}
