79f919da7f625a442c02e55771a02ac578f60cd7
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2025-02-26 18:42:34 +0000

    Two sum and LC 2011 done

--- L70/4. Two Sum.cpp
+++ L70/4. Two Sum.cpp
@@ -1,6 +1,8 @@
 //Leetcode problem no: 1
 
-/*Solution 1: Done by me */
+/*Solution 1: Done by me 
+The reason that we are starting with an empty hash map and not populating it on the start is because if we do, we might even use the same number twice.
+*/
 class Solution {
     public:
         vector<int> twoSum(vector<int>& nums, int target) {
