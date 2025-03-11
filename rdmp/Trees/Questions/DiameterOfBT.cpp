dc0d8f30c5ea582fd7e6dc18bcf9703488cde80e
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2025-03-11 17:26:11 +0000

    Diameter of BT start

---
+++ rdmp/Trees/Questions/DiameterOfBT.cpp
@@ -0,0 +1,7 @@
+/*What is the Diameter of a BT?
+- It is the longest path between any 2 nodes.
+- It is not compulsory for the path to include root.*/
+
+/* Logic:
+We can use the logic from max height of a BT. 
+- We will t*/
