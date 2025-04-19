aae9833b59907fa94772bcdb020d1676b19aafda
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2025-04-19 18:03:34 +0000

    first vid done

--- rdmp/Recursion/1.ProblemsOnRecusion.cpp
+++ rdmp/Recursion/1.ProblemsOnRecusion.cpp
@@ -1,14 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void printName(string name, int n) {
-    if (n == 0) return;
-    cout << name << endl;
-    printName(name, n - 1);
+//1. Print 1 to n linearly.
+void printNum(int num, int n) {
+    if (num > n) return;
+    cout << num << endl;
+    printNum(num+1, n);
 }
 
+//2. Print 1 to n linearly with backtrack. 
+void printNBackTrack(int num, int n)
+{
+   if(num == 0) return;
+   cout<<(n-num+1)<<endl;
+   num--;
+   printNBackTrack(num,n);
+}
+void printNto1BackTrack(int num, int n)
+{
+  if(num > n) return;
+  cout<<(n-num+1)<<endl;
+  printNto1BackTrack(num+1,n);  
+}
+
+
 int main() {
-    string name = "Nachiket";
-    printName(name, 5);
+    int n = 5;
+    int num = 1;
+   // printNBackTrack(num, n);
+    printNto1BackTrack(num,n);
     return 0;
 }
+
