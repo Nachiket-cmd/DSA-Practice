8d863f460b516a838e5a40f7dbbddad92e923dff
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2025-04-22 18:47:34 +0000

    optimised solution

--- rdmp/Recursion/5.MultipleRecCall.cpp
+++ rdmp/Recursion/5.MultipleRecCall.cpp
@@ -6,14 +6,9 @@ using namespace std;
 //We will be given N, we are supposed to give back Nth fibonnaci number in the sequence. 
 int fib(int n)
 {
-  if(n == 0) return 0;
-  else if(n == 1) return 1;
+  if(n <=1) return n;
   return (fib(n-1) + fib(n-2));
 }
-
-
-
-
 
 int main()
 {
