18515e743e33060f59100588bbfda59b9f1633ff
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2025-04-22 19:11:07 +0000

    Subsequence problem (very very very imp )

--- rdmp/Recursion/6.RecOnSubsequences.cpp
+++ rdmp/Recursion/6.RecOnSubsequences.cpp
@@ -1,3 +1,8 @@
+/*What is a subsequence?
+The sequence which can be contigous or non contigous, but strictly follows the order.
+Example: arr[3,1,2]
+Subsequences can be: {3}, {1}, {2}, {3,1}, {1,2}, {3,2}, {3,1,2} (All of these follow the order of the sequence)
+{1,2,3} wont be a subsequence.*/
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -7,5 +12,5 @@ using namespace std;
 
 int main() 
 {
-    
+
 }
