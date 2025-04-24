f07b95bba68f398d60869084d212528260ce5a6c
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2025-04-24 17:50:27 +0000

    Subsequence done very important

--- rdmp/Recursion/6.RecOnSubsequences.cpp
+++ rdmp/Recursion/6.RecOnSubsequences.cpp
@@ -5,12 +5,39 @@ Subsequences can be: {3}, {1}, {2}, {3,1}, {1,2}, {3,2}, {3,1,2} (All of these f
 {1,2,3} wont be a subsequence.*/
 #include<bits/stdc++.h>
 using namespace std;
+ 
+void printSubsequence(int count, int arr[], vector<int> &subseq, int n)
+{
+   if(count == n)
+   {
+      for(auto i : subseq) {
+         cout<<subseq[i]<<" ";
+      }
+      if(subseq.size() == 0)
+      {
+         cout<<"{}";
+      }
+      cout<<endl;
+      return;
+   }
 
+   //take or pick the particular index into the subsequence
+   subseq.push_back(arr[count]);
+   printSubsequence(count+1, arr, subseq, n); 
+   subseq.pop_back();
 
+   // not pick, or not take condition, this element is not added to your subsequence.
+   printSubsequence(count+1, arr, subseq, n); 
+}
 
 
 
 int main() 
 {
-
+   vector<int>subseq;
+   int arr[] = {3,1,2};
+   int n = 3;
+   int count = 0;
+   printSubsequence(count,arr,subseq,n);
+   return 0;
 }
