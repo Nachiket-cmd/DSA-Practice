036bac91330a1eef666b3d00443f92e2705e97eb
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2025-04-27 19:23:55 +0000

    Second variation done

--- rdmp/Recursion/7.AllKindOfPatterns.cpp
+++ rdmp/Recursion/7.AllKindOfPatterns.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void subseqwithSumK(int index, int arr[], vector<int> &subs, int sum,int k, int n)
+void allsubseqwithSumK(int index, int arr[], vector<int> &subs, int sum,int k, int n)
 {
   if(index == n)
   {
@@ -18,6 +18,39 @@ void subseqwithSumK(int index, int arr[], vector<int> &subs, int sum,int k, int 
   sum -= arr[index];
   subs.pop_back();
   subseqwithSumK(index+1, arr,subs, sum, k , n);
+}
+
+
+//Print any subsequence whose sum is sum. we need to do this not by using any boolean or conditions, we need to this by functional methods.
+/*
+- If i find a way by which my first recursion call can inform us that we found the subsequence we are looking for, we wont have to go ahead and use
+the other recursive call.
+- in the base case, if the condition is satisfied, return true else false.
+*/
+bool getoneSubseq(int index, int arr[], vector<int> &subs, int sum, int k, int n)
+{
+  if(index == n)
+  {
+    if(sum == k)
+    {
+      for(auto i:subs) cout<<i<<" ";
+      cout<<endl;
+      return true;
+    }
+    return false;
+  }
+  subs.push_back(arr[index]);
+  sum += arr[index];
+  if(getoneSubseq(index+1,arr,subs,sum,k,n)==true) 
+  {
+   return true;
+  }
+  subs.pop_back();
+  sum -= arr[index];
+  if(getoneSubseq(index+1,arr,subs,sum,k,n)==true) return true;
+  {
+    return false;
+  }
 }
 
 
