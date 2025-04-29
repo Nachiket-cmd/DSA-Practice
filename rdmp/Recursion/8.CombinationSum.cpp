be4a0e67f7aff27ee4f33b4c5e638942c7f4fa6a
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2025-04-29 15:15:28 +0000

    combination sum done

--- rdmp/Recursion/8.CombinationSum.cpp
+++ rdmp/Recursion/8.CombinationSum.cpp
@@ -1,37 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> combinationSum(int index, vector<int>arr, vector<int>subs, vector<vector<int>>ans, int target)
+void combinationSum(int index, vector<int>arr, vector<int>subs, vector<vector<int>>ans,int target)
 {
     if(index == arr.size())
     {
         if(target == 0)
         {
-            ans.push(subs);
+            ans.push_back(subs);
         }
         return;
     }
-
-    if(arr[index]<=target)
+    if(arr[index] <= target)
     {
         subs.push_back(arr[index]);
-        combinationSum(index,arr,subs,ans,target-arr[index]);
-    }
-    else {
+        combinationSum(index,arr,subs,ans,target - arr[index]);
         subs.pop_back();
-        combinationSum(index+1,arr,subs,ans,target+arr[index]);
     }
-
+    combinationSum(index+1, arr, subs, ans, target);
 }
 
 int main() {
  vector<vector<int>>ans;
  vector<int>subs;
  vector<int>arr = {2,3,6,7};
- ans = combinationSum(0,arr,subs,ans,7);
- for(auto i:ans)
-  {
-    cout<<i;
-  }
+ combinationSum(0,arr,subs,ans,7);
+ for (const auto& i : ans) {
+    for (int val : i) {
+        cout << val << " ";
+    }
+    cout << endl;
+}
   return 0;
 }
